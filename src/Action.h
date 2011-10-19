8f47933ba70d06c2be4aa5018f6ab90c2c1873ff
daole <daole@f58d9e53-517f-4ec4-bd6d-b140a30fe857>
2011-10-19 06:25:34 +0000

    Document Action.h
    git-svn-id: http://bigbird.comp.nus.edu.sg/motion/proj/mcvi/trunk@65 f58d9e53-517f-4ec4-bd6d-b140a30fe857

--- src/Action.h
+++ src/Action.h
@@ -8,6 +8,11 @@ enum actType{Initial, Macro, Act, None}; // types of actions
 class Action
 {
   public:
+    // actNum is for internal use (arrange as InitActs, MacroActs,
+    // NormalActs), no need for type.
+
+    // actNumUser is for the user (type + index of the action with
+    // this type)
     long actNum, actNumUser;
     actType type;
     static Model* model;
@@ -20,23 +25,48 @@ class Action
 
     static int compare(const Action& a, const Action& b);
 
-   void setActNum(long actNum);
-   void setActNumUser(actType type, long actNumUser);
+    /**
+       Set actNum, compute the type and actNumUser
+       @param [in] actNum
+    */
+    void setActNum(long actNum);
 
     /**
-       Get the type of action given its index in the \a actEdges
+       Set type, actNumUser, compute actNum
+       @param [in] type
+       @param [in] actNumUser
+    */
+    void setActNumUser(actType type, long actNumUser);
+
+    /**
+       Get the type of action, computed using actNum
     */
     actType getActType();
 
+    /**
+       Get actNumUser
+    */
     long getActNumUser() const;
 
+    /**
+       Compute the type based on actNum
+    */
     void computeType();
 
     /**
-       Given the action type \a aType and action number \a aNum, return
-       the position in \a actEdges.
+       Given the type \a aType and actNumUser number \a aNum, return
+       the actNum.
+       @param [in] model
+       @param [in] aType : the type
+       @param [in] aNum  : actNumUser number
+       @return actNum
     */
     static long getBeliefAct(Model& model, actType aType, long aNum);
+
+    /**
+       Set the model for querying number of InitActs, MacroActs
+       @param [in] model
+    */
     static void initStatic(Model* model);
 };
 
