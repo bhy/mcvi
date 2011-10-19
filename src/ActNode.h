b5053b22585ecddf33edfdd2b544ac10604c0d4b
daole <daole@f58d9e53-517f-4ec4-bd6d-b140a30fe857>
2011-10-19 06:25:35 +0000

    Documenting ActNode
    git-svn-id: http://bigbird.comp.nus.edu.sg/motion/proj/mcvi/trunk@66 f58d9e53-517f-4ec4-bd6d-b140a30fe857

--- src/ActNode.h
+++ src/ActNode.h
@@ -14,14 +14,35 @@ class ActNode
   public:
     ActNode(const Action& action, const Belief& belief, Bounds* bounds);
 
+    /**
+       Back up on this ActNode
+    */
     void backup();
+
+    /**
+       Sample observations and create corresponding ObsEdge.
+
+       Each ObsEdge will have a number of particles corresponding to
+       the probability of getting the observation.
+    */
     void generateObsPartitions();
+
+    /**
+       Remove all the generated particles from all the ObsEdge
+    */
     void clearObsPartitions();
 
+    // The label of this ActNode
     const Action& action;
+    // The belief representation of the Belief father of this ActNode
     const Belief& belief;
     Bounds* bounds;
+    // The average lower (upper) bounds compute by dividing the total
+    // lower (upper) bounds of all ObsEdge children by the number of
+    // such edges
     double avgLower, avgUpper;
+    // The randSeed for generateing ObsEdge, so if we redo the
+    // generation, it will be the same
     unsigned long randSeed;
     std::map<Obs, ObsEdge> obsChildren;
 };
