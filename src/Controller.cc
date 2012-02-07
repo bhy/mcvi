b092fd66657dbc0a57cff328e29d133c6124e0f2
daole <daole@f58d9e53-517f-4ec4-bd6d-b140a30fe857>
2012-02-07 05:00:53 +0000

    Finalization of Controller
    git-svn-id: http://bigbird.comp.nus.edu.sg/motion/proj/mcvi/trunk@144 f58d9e53-517f-4ec4-bd6d-b140a30fe857

--- src/Controller.cc
+++ src/Controller.cc
@@ -1,23 +1,49 @@
 #include "Controller.h"
+#include "Model.h"
+#include "Belief.h"
+#include "Action.h"
+#include "Obs.h"
+#include <iostream>
 
 using namespace std;
 
 Controller::Controller(PolicyGraph& policy, Belief* belief):
-        policy(policy)
+        policy(policy),
+        currBel(belief),
+        firstAction(true)
 {
     currGraphNode = policy.getRoot(0);
 }
 
-Controller::Controller(PolicyGraph& policy, Model& model):
-        policy(policy)
+Controller::Controller(PolicyGraph& policy, Model const& model):
+        policy(policy),
+        currBel(model.initialBelief()),
+        firstAction(true)
 {
     currGraphNode = policy.getRoot(0);
 }
 
-Action& Controller::nextAction(Obs const& obs)
+Action const& Controller::nextAction(Obs const& obs, int dummy)
 {
-    Action& action =  policy.getAction(currGraphNode);
-    currGraphNode = policy.getNextState(currGraphNode, obs);
+    if (firstAction) {
+        // If this is the first action then ignore the observation
+        firstAction = false;
+        return policy.getAction(currGraphNode);
+    }
 
-    return action;
+    Action& action = policy.getAction(currGraphNode);
+    PolicyGraph::Node *nextGraphNode = policy.getNextState(currGraphNode, obs);
+    currGraphNode = nextGraphNode;
+
+    // Update the belief
+    Belief* nextBelief = currBel->nextBelief(action, obs);
+    delete currBel;
+    currBel = nextBelief;
+
+    return policy.getAction(currGraphNode);;
+}
+
+Belief const* Controller::currBelief() const
+{
+    return currBel;
 }
--- src/Controller.h
+++ src/Controller.h
@@ -1,23 +1,40 @@
+/**
+   @brief An implementation of the controller
+   @author Le Trong Dao
+   @date 2012-02-05
+**/
+
 #ifndef __CONTROLLER_H
 #define __CONTROLLER_H
 
-#include "Model.h"
-#include "PolicyGraph.h"
+// Some macros to adapt to the interface
+#define ActionDefine Action const&
+#define ObsDefine Obs const&
+#define BeliefDefine Belief const*
 
+class Model;
 class Belief;
+class Action;
+class Obs;
+
+#include "ControllerInterface.h"
+#include "PolicyGraph.h"
 
 // This class will maintain an internal belief and specify the next
 // action given the current observation
-class Controller
+class Controller: public ControllerInterface
 {
   public:
     Controller(PolicyGraph& policy, Belief* belief);
-    Controller(PolicyGraph& policy, Model& model);
-    Action& nextAction(Obs const& obs);
+    Controller(PolicyGraph& policy, Model const& model);
+    ActionDefine nextAction(ObsDefine obs, int dummy = -1);
+    BeliefDefine currBelief() const;
 
   private:
     PolicyGraph& policy;
     PolicyGraph::Node *currGraphNode;
+    Belief* currBel;
+    bool firstAction;
 };
 
 #endif
